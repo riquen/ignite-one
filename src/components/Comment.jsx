6fe5999c843ff398968f63d616f61e730afaa150
Henrique Nunes <henri.arantes1@gmail.com>
2023-08-30 22:03:08 -0300

    concluding layout

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,10 +1,11 @@
 import { ThumbsUp, Trash } from 'phosphor-react'
 import styles from './Comment.module.css'
+import { Avatar } from './Avatar'
 
 export function Comment() {
     return (
         <div className={styles.comment}>
-            <img className={styles.avatar} src="https://github.com/riquen.png" />
+            <Avatar hasBorder={false} src="https://github.com/tiagofsv95.png" />
             <div className={styles.commentBox}>
                 <div className={styles.commentContent}>
                     <header>
@@ -13,7 +14,7 @@ export function Comment() {
                             <time title='11 de Maio às 08:13h' dateTime='2022-05-11 08:13:30'>Cerca de 1h atrás</time>
                         </div>
                         <button title='Deletar comentário'>
-                            <Trash size={20} />
+                            <Trash size={24} />
                         </button>
                     </header>
                     <p>Muito bom Devon, parabéns!! 👏👏</p>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,7 @@
 import { PencilLine } from 'phosphor-react'
 
 import styles from './Sidebar.module.css'
+import { Avatar } from './Avatar'
 
 export function Sidebar() {
     return(
@@ -10,7 +11,7 @@ export function Sidebar() {
                 src="https://images.unsplash.com/photo-1605379399642-870262d3d051?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=50"
             />
             <div className={styles.profile}>
-                <img className={styles.avatar} src="https://github.com/riquen.png" />
+                <Avatar src="https://github.com/riquen.png" />
                 <strong>Henrique Arantes</strong>
                 <span>Web Developer</span>
             </div>
